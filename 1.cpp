ff844190a0c1d99d3e0cb71fb8e62c79c8c2bc87
DEZOIST <43541396+DEZOIST@users.noreply.github.com>
2022-09-12 16:09:02 +0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,107 @@
+// 欧几里得
+ll gcd(ll a, ll b) {
+    if (b == 0) return a;
+    else return gcd(b, a % b);
+}
+
+// 快速幂
+ll powmod(ll a, ll b, ll mod) {
+    ll ret = 1;
+    for (; b; b >>= 1) {
+        if (b & 1) ret = ret * a % mod;
+        a = a * a %mod;
+    }
+    return ret;
+}
+
+
+// 扩展欧几里得
+ll exgcd(ll a, ll b, ll &x, ll &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    ll d = exgcd(b, a % b, y, x);
+    y -= (a / b) * x;
+    return d;
+}
+
+// 求 a * x = b (mod m) 的解
+ll modequ(ll a, ll b, ll m) {
+    ll x, y;
+    ll d = exgcd(a, m, x, y);
+    if (b % d != 0) return -1;
+    m /= d; a /= d; b /= d;
+    x = x * b % m;
+    if (x < 0) x += m;
+    return x;
+}
+
+
+// 合并两个同余方程
+void merge(ll &a, ll &b, ll c, ll d) { // d <= 10^9
+    // bt = c - a(mod d)
+    if (a == -1 && b == -1) return;
+    ll x, y;
+    ll g = exgcd(b, d, x, y);
+    //bx = g(mod d)
+    if ((c - a) % g != 0) {
+        a = b = -1;
+        return;
+    }
+    d /= g; // d'
+    ll t0 = ((c - a) / g) % d * x % d;
+    if (t0 < 0) t0 += d;
+    // t = t0 (mod d')
+    a = b * t0 + a;
+    b = b * d;
+}
+
+// 线性筛
+    p[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        if (!p[i]) p[i] = i, pr[++tot] = i;
+        for (int j = 1; j <= tot && pr[j] * i <= n; j++) {
+            p[i * pr[j]] = pr[j];
+            if (p[i] == pr[j]) break;
+        }
+    }
+
+nt gauss()  // 高斯消元，答案存于a[i][n]中，0 <= i < n
+{
+    int c, r;
+    for (c = 0, r = 0; c < n; c ++ )
+    {
+        int t = r;
+        for (int i = r; i < n; i ++ )  // 找绝对值最大的行
+            if (fabs(a[i][c]) > fabs(a[t][c]))
+                t = i;
+
+        if (fabs(a[t][c]) < eps) continue;
+
+        for (int i = c; i <= n; i ++ ) swap(a[t][i], a[r][i]);  // 将绝对值最大的行换到最顶端
+        for (int i = n; i >= c; i -- ) a[r][i] /= a[r][c];  // 将当前行的首位变成1
+        for (int i = r + 1; i < n; i ++ )  // 用当前行将下面所有的列消成0
+            if (fabs(a[i][c]) > eps)
+                for (int j = n; j >= c; j -- )
+                    a[i][j] -= a[r][j] * a[i][c];
+
+        r ++ ;
+    }
+
+    if (r < n)
+    {
+        for (int i = r; i < n; i ++ )
+            if (fabs(a[i][n]) > eps)
+                return 2; // 无解
+        return 1; // 有无穷多组解
+    }
+
+    for (int i = n - 1; i >= 0; i -- )
+        for (int j = i + 1; j < n; j ++ )
+            a[i][n] -= a[i][j] * a[j][n];
+
+    return 0; // 有唯一解
+}
+
